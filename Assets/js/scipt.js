27f082616217366c585e63d31ba111b7d2b4b832
Daniel Mrva <mrva.paints@gmail.com>
2022-02-02 21:45:18 -0700

    worked out moon phase object

---
+++ Assets/js/scipt.js
@@ -0,0 +1,44 @@
+console.log("Greetings")
+
+moonPhase = [
+    {
+        phase: "new moon",
+        mood: "new beginnings",
+        activity: [],
+    },
+    {
+        phase: "waxing crescent",
+        mood: "set intetnions",
+        activity: [],
+    },
+    {
+        phase: "first quarter",
+        mood: "take action",
+        activity: [],
+    },
+    {
+        phase: "waxing gibbous",
+        mood: "Refine and home",
+        activity: [],
+    },
+    {
+        phase: "full",
+        mood: "Harvest Endeavors",
+        activity: [],
+    },
+    {
+        phase: "waning gibbous",
+        mood: "introspect",
+        activity: [],
+    },
+    {
+        phase: "third quarter",
+        mood: "relase and let go",
+        activity: [],
+    },
+    {
+        phase: "waning crescent",
+        mood: "surrender",
+        activity: [],
+    }
+]
