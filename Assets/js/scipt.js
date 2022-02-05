8fbe30d9fce4cd4468ae95f10323853cbf8932c5
Daniel Mrva <mrva.paints@gmail.com>
2022-02-05 10:12:49 -0700

    pseudo coded

--- Assets/js/scipt.js
+++ Assets/js/scipt.js
@@ -1,6 +1,46 @@
-console.log("Greetings")
 
-moonPhase = [
+/**
+ * Dan's to-do
+ * 
+ * PSEUDOCODE
+ * 
+ * variables for moonphases, activities types, logic for datepickers, 
+ * 
+ * UI:
+ *  USER - Select Date (Default: now) Picker: https://materializecss.com/pickers.html
+ *  USER - Select Participants (Dropdown?)
+ *  USER - Select Price (Dropdown?)
+ *  USER - Find Activities Button
+ *              
+ * INIT
+ *  Query Local storage for saved activities, then RENDER
+ * 
+ * FETCH
+ *  MoonPhase at Select Date
+ *  Return MoonPhase
+ *      Returned Moonphase sets activities availbile
+ *      Random number to select activity use some sort of weighted bell curve
+ *          (Math.floor((Math.random * x) + (Math.random * y)) -1) Maximum sum of x+y-1 should be equal to activitiesArray.length
+ *      Return selected activity(s)
+ * 
+ * FETCH
+ *  Bored ?={Participants}&{Price}&{Type}
+ * 
+ * RENDER
+ *  List of datapoints pulled from return of Bored API results
+ * 
+ * STORE
+ *  With button save rendered datapoints to local storage
+ *  On same button, add datapoints to an array
+ * 
+ * 
+ * 
+ * 
+ */
+
+console.log("Greetings");
+
+const moonPhase = [
     {
         phase: "new moon",
         mood: "new beginnings",
@@ -18,7 +58,7 @@ moonPhase = [
     },
     {
         phase: "waxing gibbous",
-        mood: "Refine and home",
+        mood: "Refine and hone",
         activity: [],
     },
     {
@@ -41,4 +81,5 @@ moonPhase = [
         mood: "surrender",
         activity: [],
     }
-]
+];
+
